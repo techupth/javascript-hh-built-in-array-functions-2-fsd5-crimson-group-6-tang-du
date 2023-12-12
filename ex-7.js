2f5e5328f5291568ee9f026c8c7917eb4624644a
unknown <tb.poopp@gmail.com>
2023-12-12 18:29:50 +0700

    feat: add exercise built in

--- ex-7.js
+++ ex-7.js
@@ -1,8 +1,10 @@
 function isPalindrome(string) {
   // Start coding here
+  const checkWords = string.split("").reverse().join("");
+  return string === checkWords;
 }
 
 //Example case
-console.log(isPalindrome("reviver"));// true
-console.log(isPalindrome("บวบ"));// true
-console.log(isPalindrome("deliver"));// false
+console.log(isPalindrome("reviver")); // true
+console.log(isPalindrome("บวบ")); // true
+console.log(isPalindrome("deliver")); // false
