3d998a79bd4c645e3bca4a36228fb57a03257ead
Tanawut Wongboot <tanawutwongboot@Mac-mini-khxng-Tanawut.local>
2023-12-12 15:09:18 +0700

    feat(ex-4): using reduce to find unique name in array of object

--- ex-4.js
+++ ex-4.js
@@ -374,4 +374,7 @@ const bills = [
 ];
 
 // Start coding here
-const totalMembers;
+const totalMembers = bills
+  .filter((bill) => bill.member !== null)
+  .map((bill) => bill.member.name);
+console.log(`Unique Members Count: ${totalMembers}`);
